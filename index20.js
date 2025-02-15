bbd0ec28e5cf0fe99efcdffa58e194c7d1629fcb
Priyanksu <140146085+Priyanksu28@users.noreply.github.com>
2025-02-15 16:35:22 +0530

    committed

--- index20.js
+++ index20.js
@@ -3,4 +3,28 @@
 // console.log(descripter);
 
 
-console.log("Math.PI", '5');
+const chai = {
+    name: 'Priyanksu',
+    price: 100,
+    isAvailalbe: true,
+
+    orderchai: () => {
+        console.log("no chai");    
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    writable: false,
+    enumerable: false,
+    configurable: false
+})
+ 
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value != 'function') {
+        console.log(`${key} : ${value}`);
+    }    
+}
